c666cea75c05e1c7842f6adf19689c57013bd6ea
hristoborisov <raxevsky@gmail.com>
2014-09-25 23:31:00 +0300

    Setup of the first screen

--- scripts/MainController.js
+++ scripts/MainController.js
@@ -1,31 +1,20 @@
 ﻿app.controller("MainController", [
     "$scope", "driversService", "$location", "$anchorScroll",
     function ($scope, driversService, $location, $anchorScroll) {
-        $scope.tasks = [
-               { title: 'Collect coins' },
-               { title: 'Eat mushrooms' },
-               { title: 'Get high enough to grab the flag' },
-               { title: 'Find the Princess' }
-        ];
 
-        function initialization() {
-
-            $scope.registration = "CA7670PT";
-
+        $scope.onCameraClick = function () {
+            navigator.camera.getPicture(cameraSuccess, cameraError, {
+                quality: 50,
+                destinationType: Camera.DestinationType.DATA_URL
+            });
         }
 
-        $scope.onSearchUserClick = function () {
-
-            var licenseSearch = driversService.returnValidLicensePlate($scope.registration);
-
-            if (licenseSearch != '') {
-                $scope.invalidSearch = false;
-                $location.path("/driver/" + licenseSearch);
-            }
-            else {
-                $scope.invalidSearch = true;
-            }
+        function cameraSuccess(imageData) {
+            var image = document.getElementById('myImage');
+            image.src = "data:image/jpeg;base64," + imageData;
         }
 
-        initialization();
+        function cameraError(message) {
+            alert('Failed because: ' + message);
+        }
     }]);
