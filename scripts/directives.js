7ddd5afd2ba87089778bfc803596f5a549e5a2fe
Hristo Borisov <hristoborisov@Hristos-MBP.tbc.bg>
2014-10-18 23:53:23 +0300

    Forgot to commit the new directives.js file

---
+++ scripts/directives.js
@@ -0,0 +1,15 @@
+(function(){
+    var app = angular.module("Rednecks");
+    app.directive('ngEnter', function () {
+        return function (scope, element, attrs) {
+            element.bind("keydown keypress", function (event) {
+                if(event.which === 13) {
+                    scope.$apply(function (){
+                        scope.$eval(attrs.ngEnter);
+                    });
+                    event.preventDefault();
+                }
+            });
+        };
+    });
+})();
